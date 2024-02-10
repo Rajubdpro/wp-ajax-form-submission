7d7be6f857e4b07d64187535ac066ba16ded00c7
Shamim Hasan <91910050+Rajubdpro@users.noreply.github.com>
2024-02-10 21:09:35 +0600

    Update form

---
+++ admin/template/form.php
@@ -0,0 +1,29 @@
+<form id="afs-form" action="afs_form_submit">
+    <label for="name">Name</label>
+    <input class="form-control" type="text" id="name" name="name" placeholder="Enter Your Name">
+    <br>
+    <br>
+    <label for="email">Email</label>
+    <input class="form-control" type="email" name="email"  id="email" placeholder="Enter Your Email">
+    <br>
+    <br>
+    <label for="phone">Phone</label>
+    <input class="form-control" type="number" id="phone" name="phone" placeholder="Your phone number">
+    <br>
+    <br>
+    <label for="password">Password</label>
+    <input class="form-control" type="password" id="password" name="password" placeholder="Your Password">
+    <br>
+    <br>
+    <label for="subject">Subject</label>
+    <label for="subject">Choose Your Subject</label>
+    <select class="form-control" id="subject" name="subject" form="subjectform">
+        <option value="bangla">bangla</option>
+        <option value="english">english</option>
+        <option value="socilogy">socilogy</option>
+        <option value="history">history</option>
+    </select>
+    <br>
+    <br>
+    <input type="submit" name="submit" id="afs_form_submit" value="submit">
+</form>
--- assets/js/afs-script.js
+++ assets/js/afs-script.js
@@ -6,14 +6,16 @@ jQuery(document).ready(function($) {
     $("#afs-form").submit(function(e) {
         e.preventDefault();
         var afs_form = $(this).serialize();
+        console.log(afs_form);
         $.ajax({
             url: afs_ajax.ajax_url,
             type: 'post',
+            dataType:'json',
             data: {
-                action: 'afs_form_submit',
-                afs_form: afs_form
+                action: 'afs_form_submit', ajax_data:afs_form,
             },
             success: function(data) {
+                console.log(data);
                 $("#afs-form")[0].reset();
                 $("#afs-form").append('<div class="afs-success">Form Submitted Successfully</div>');
                 setTimeout(function() {
